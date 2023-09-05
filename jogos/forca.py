e330e17691d2d7bc7bc04a53339e9b899080fd5b
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2023-09-05 12:55:16 -0300

    Iniciei o projeto jogo.forca

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,6 +3,24 @@ def jogar():
     print("===Bem-vindo ao jogo da forcar!===")
     print("==================================")
 
+    palavra_secreta = "moreno"
+    
+    enforcou = False
+    acertou = False
+
+    #enquanto(True) 
+    while(not enforcou and not acertou):
+
+        chute = input("Digite uma letra: ")
+        chute = chute.strip()
+
+        index = 0 #posição
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("encontrei a letra {} na posição {} " .format(chute, index))
+            index += 1
+        
+        print("Jogando...")
     print("Fim de jogo")
 
 if(__name__ == "__main__"):
