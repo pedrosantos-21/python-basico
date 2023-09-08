708aed17198762b57782fa167d1d387b1e195fa3
pedro vinicius <128748644+pedrosantos-21@users.noreply.github.com>
2023-09-08 13:57:26 -0300

    Finalizei o código

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,25 +3,41 @@ def jogar():
     print("===Bem-vindo ao jogo da forcar!===")
     print("==================================")
 
-    palavra_secreta = "moreno"
-    
+    palavra_secreta = "moreno".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
     enforcou = False
     acertou = False
+    erros = 0
 
-    #enquanto(True) 
-    while(not enforcou and not acertou):
+    print(letras_acertadas)
 
+    # enquanto(True)
+    while (not enforcou and not acertou):
         chute = input("Digite uma letra: ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0 #posição
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("encontrei a letra {} na posição {} " .format(chute, index))
-            index += 1
-        
-        print("Jogando...")
+        if (chute in palavra_secreta):
+            index = 0  # posição
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("Você errou, restam {} tentativas".format(6 - erros))
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if(acertou):
+        print("Você ganhou!")
+
+    else:
+        print("Você perdeu!")
     print("Fim de jogo")
 
-if(__name__ == "__main__"):
+
+if (__name__ == "__main__"):
     jogar()
